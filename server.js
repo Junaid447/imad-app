5dff36fa7832adac76e7f62aad2812829f88789f
Junaid447 <30664150+Junaid447@users.noreply.github.com>
2017-08-13 21:08:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,19 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'article-one.html'));  
-});
-
-app.get('/article-two',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'article-two.html'));  
-});
-app.get('/article-three',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'article-three.html'));  
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
