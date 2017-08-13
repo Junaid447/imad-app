45d70082ab74176caf57c1a806172f990117426e
Junaid447 <30664150+Junaid447@users.noreply.github.com>
2017-08-13 21:34:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+var img=document.getElementById('madi');
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,100);
+    
+};
